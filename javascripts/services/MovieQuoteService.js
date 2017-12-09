9c3e8be48c4e307016b4c9fddd3bb210f6d56e42
David Dassau <todieahero@hotmail.com>
2017-12-09 11:30:12 -0600

    still working on getting quotes to save

--- javascripts/services/MovieQuoteService.js
+++ javascripts/services/MovieQuoteService.js
@@ -19,6 +19,29 @@ app.service("MovieQuoteService", function($http, $q, FIREBASE_CONFIG){
         });
     };
 
+    // Try it out
+    const getMovieQuoteForSaved = (uid) => {
+        let savedMovieQuote = [];
+        console.log("uid", uid);
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/movieQuotes.json?orderBy="uid"&equalTo="${uid}"`).then((results) => {
+                let fbSavedMovieQuotes = results.data;
+                Object.keys(fbSavedMovieQuotes).forEach((key) => {
+                    if (fbSavedMovieQuotes[key].isFavorited) {
+                        fbSavedMovieQuotes[key].id = key;
+                        savedMovieQuote.push(fbSavedMovieQuotes[key]);
+                    }
+                });
+                console.log("savedMovieQuote", savedMovieQuote);
+                resolve(savedMovieQuote);
+                
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    };
+    // end Try it out
+
     const searchMovieQuotes = (query) => {
         let queriedMovieQuote = [];
         
@@ -51,6 +74,6 @@ app.service("MovieQuoteService", function($http, $q, FIREBASE_CONFIG){
         };
     };
 
-    return {getMovieQuoteFromDB, searchMovieQuotes, updateMovieQuote, createMovieQuoteObject};
+    return { getMovieQuoteFromDB, searchMovieQuotes, updateMovieQuote, createMovieQuoteObject, getMovieQuoteForSaved};
 
 });
