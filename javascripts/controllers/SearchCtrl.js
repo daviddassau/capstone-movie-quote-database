468a609ac8bf8b745e1665e31d79d9be19f2478e
David Dassau <todieahero@hotmail.com>
2017-12-16 09:09:02 -0600

    message comes back when no quote is found

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -4,11 +4,17 @@ app.controller("SearchCtrl", function ($location, $scope, MovieQuoteService, Mov
 
     $scope.movies = [];
     $scope.movieIds = [];
+    $scope.quotesDisplay = -1;
 
     $scope.enterPush = (event) => {
         if (event.keyCode === 13) {
             MovieQuoteService.searchMovieQuotes(event.target.value).then((results) => {
-                getMovies(results);
+                if (results.length === 0){
+                    $scope.quotesDisplay = 0;
+                } else {
+                    $scope.quotesDisplay = 1;
+                    getMovies(results);
+                }
             }).catch((err) => {
                 console.log("error in searchMovies", err);
             });
